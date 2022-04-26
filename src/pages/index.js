270318fe4881ffa1b45ee6d476065908472905e3
Owini <owiniose@gmail.com>
2022-04-26 08:49:28 +0100

    3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
+
 import Line from '../components/Roadmap/Line';
 import Roadmap from '../components/Roadmap/Roadmap';
 
 import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
   return (
